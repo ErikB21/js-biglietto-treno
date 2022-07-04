a8cf3b8c572bca4e14a46c76d5da4afbb6f98916
ErikBorgogno <erikborgo.20@gmail.com>
2022-07-04 17:33:18 +0200

    add output .toFixed for number

--- js/main.js
+++ js/main.js
@@ -5,6 +5,10 @@ const prezzoTotale = numeroKilometri * prezzoKilometro;
 const scontoRagazzi = (prezzoTotale * 0.8);
 const scontoAnziani = (prezzoTotale * 0.6);
 
+let n = scontoRagazzi.toFixed(2);
+let nu = scontoAnziani.toFixed(2);
+let num = prezzoTotale.toFixed(2);
+
 
 
 if (numeroKilometri >= 1){
@@ -12,15 +16,15 @@ if (numeroKilometri >= 1){
     if (età < 18){
         const scontoRagazzi= prezzoTotale * 0.8;
         console.log(scontoRagazzi);
-        document.getElementById('totTravel').innerHTML = 'Il prezzo totale è' + '' + scontoRagazzi + '€';
+        document.getElementById('totTravel').innerHTML = 'Il prezzo totale è' + '' + n + '€';
     } else if (età >= 65){
         const scontoAnziani = prezzoTotale * 0.6;
         console.log(scontoRagazzi);
-        document.getElementById('totTravel').innerHTML = 'Il prezzo totale è' + '' + scontoAnziani + '€';
+        document.getElementById('totTravel').innerHTML = 'Il prezzo totale è' + '' + nu + '€';
     }else{
         const prezzoTotale = numeroKilometri * prezzoKilometro;
         console.log(prezzoTotale);
-        document.getElementById('totTravel').innerHTML = 'Il prezzo totale è' + '' + prezzoTotale + '€';
+        document.getElementById('totTravel').innerHTML = 'Il prezzo totale è' + '' + num + '€';
     }
 } else{
     const numeroKilometri = parseInt(prompt('Richiedi il numero di Km'));
